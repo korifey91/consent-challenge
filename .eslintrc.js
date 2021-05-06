23e4704ab159803505e2d6a2bd4ea0b78ae1ab50
Kirill Efimov <k.efimov@dunice.net>
2021-05-06 21:19:10 +0300

    Prepare project

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  plugins: ['eslint-plugin-import-helpers'],
+  rules: {
+    'import-helpers/order-imports': [
+      'error',
+      {
+        groups: [
+          '/^@?react/',
+          'module',
+          ['parent', 'sibling', 'index'],
+          '/.*(css|scss)$/',
+        ],
+        newlinesBetween: 'always',
+      },
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
