50136f78e7fcb7d14e9fee3ce43a320f9fddb9aa
Kirill Efimov <k.efimov@dunice.net>
2021-05-07 17:49:27 +0300

    set up tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { unmountComponentAtNode } from 'react-dom';
 
 import App from './App';
+import configureStore from 'redux-mock-store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockStore = configureStore([]);
+
+let container = null;
+let store = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  store = mockStore({
+    myState: 'sample text',
+  });
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('renders whole app correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
