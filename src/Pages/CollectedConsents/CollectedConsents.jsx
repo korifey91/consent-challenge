b0a3877168107dfc45ebaf3c4ff875baf1d55e1f
Kirill Efimov <k.efimov@dunice.net>
2021-05-07 14:52:56 +0300

    Clean CollectedConsents component

--- src/Pages/CollectedConsents/CollectedConsents.jsx
+++ src/Pages/CollectedConsents/CollectedConsents.jsx
@@ -19,31 +19,6 @@ const columns = [
   { id: 'consent', label: 'Consent given for', minWidth: 250 },
 ];
 
-// const rows = [
-//   {
-//     id: 1,
-//     name: 'User Friendly',
-//     email: 'user@friendly.com',
-//     consents: ['Receive newsletter'],
-//   },
-//   {
-//     id: 2,
-//     name: 'User2 Friendly',
-//     email: 'user2@friendly.com',
-//     consents: ['Receive newsletter', 'Be shown targeted ads'],
-//   },
-//   {
-//     id: 3,
-//     name: 'User3 Friendly',
-//     email: 'user3@friendly.com',
-//     consents: [
-//       'Receive newsletter',
-//       'Be shown targeted ads',
-//       'Contribute to anonymous visit statistics',
-//     ],
-//   },
-// ];
-
 const rowsPerPage = 2;
 
 function CollectedConsents() {
